b639bcac52b4e369db08f47a5a2ad3b4fd0f0bdd
petracvrljevic <pcvrljevic@gmail.com>
2019-06-19 11:54:39 +0200

    Changing plan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,6 +32,11 @@ module ApplicationHelper
     Tenant.find(tenant_id).name
   end
 
+  def class_name_for_tenant_form(tenant)
+    return "cc_form" if tenant.payment.blank?
+    ""
+  end
+
   def s3_link(tenant_id, artifact_key)
     link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
   end
